daca35bf53d5df23941cf3f0bce1fe5c78df03f0
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-03-29 18:23:14 +0200

    Update PlayerJoinListener.java

--- GItHubSpigotCoding/src/de.derkaottv/listeners/PlayerJoinListener.java
+++ GItHubSpigotCoding/src/de.derkaottv/listeners/PlayerJoinListener.java
@@ -1,19 +1,38 @@
 package de.derkaottv.listeners;
 
+import de.derkaottv.utils.Scoreboard;
+import de.derkaottv.utils.getItems;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerJoinListener implements Listener {
+
+    private de.derkaottv.main plugin;
+
+    public PlayerJoinListener(de.derkaottv.main main) {
+        this.plugin = main;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         String name = p.getName();
-        
+
         getItems.getMainItems( p );
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for( Player all : Bukkit.getOnlinePlayers() ) {
+                    Scoreboard.setScoreboard( all );
+                }
+            }
+        }.runTaskLater( plugin, 1 );
 
         if( p.isOp() ) {
 
