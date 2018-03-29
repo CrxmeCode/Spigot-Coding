72fe392734464ab255b22d3149fc7c78e24bac9e
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-03-29 20:56:42 +0200

    Update GameModeCommand.java

--- GItHubSpigotCoding/src/de.derkaottv/commands/GameModeCommand.java
+++ GItHubSpigotCoding/src/de.derkaottv/commands/GameModeCommand.java
@@ -16,6 +16,7 @@ public class GameModeCommand implements CommandExecutor {
             if( args.length == 1 ) {
                 if( args[0].equalsIgnoreCase( "0" ) || args[0].equalsIgnoreCase( "survival" ) ) {
                     if( ! ((Player) sender).getGameMode().equals( GameMode.SURVIVAL ) ) {
+                        
                         ((Player) sender).setGameMode( GameMode.SURVIVAL );
                         sender.sendMessage( "§fYou're now in the §6" + ((Player) sender).getGameMode() + " §fmode." );
                     } else {
@@ -24,6 +25,7 @@ public class GameModeCommand implements CommandExecutor {
                 }
                 if( args[0].equalsIgnoreCase( "1" ) || args[0].equalsIgnoreCase( "creative" ) ) {
                     if( ! ((Player) sender).getGameMode().equals( GameMode.CREATIVE ) ) {
+
                         ((Player) sender).setGameMode( GameMode.CREATIVE );
                         sender.sendMessage( "§fYou're now in the §6" + ((Player) sender).getGameMode() + " §fmode." );
                     } else {
@@ -32,6 +34,7 @@ public class GameModeCommand implements CommandExecutor {
                 }
                 if( args[0].equalsIgnoreCase( "2" ) || args[0].equalsIgnoreCase( "adventure" ) ) {
                     if( ! ((Player) sender).getGameMode().equals( GameMode.ADVENTURE ) ) {
+
                         ((Player) sender).setGameMode( GameMode.ADVENTURE );
                         sender.sendMessage( "§fYou're now in the §6" + ((Player) sender).getGameMode() + " §fmode." );
                     } else {
@@ -40,6 +43,7 @@ public class GameModeCommand implements CommandExecutor {
                 }
                 if( args[0].equalsIgnoreCase( "3" ) || args[0].equalsIgnoreCase( "spectator" ) ) {
                     if( ! ((Player) sender).getGameMode().equals( GameMode.SPECTATOR ) ) {
+
                         ((Player) sender).setGameMode( GameMode.SPECTATOR );
                         sender.sendMessage( "§fYou're now in the §6" + ((Player) sender).getGameMode() + " §fmode." );
                     } else {
@@ -52,6 +56,7 @@ public class GameModeCommand implements CommandExecutor {
                 if( target != null ) {
                     if( args[0].equalsIgnoreCase( "0" ) || args[0].equalsIgnoreCase( "survival" ) ) {
                         if( ! target.getGameMode().equals( GameMode.SURVIVAL ) ) {
+
                             target.setGameMode( GameMode.SURVIVAL );
                             target.sendMessage( "§fYour Gamemode was updated!" );
                             sender.sendMessage( "§fThe player §6" + target.getName() + " §fis now in the §6" + target.getGameMode() + " §fmode." );
@@ -61,6 +66,7 @@ public class GameModeCommand implements CommandExecutor {
                     }
                     if( args[0].equalsIgnoreCase( "1" ) || args[0].equalsIgnoreCase( "creative" ) ) {
                         if( ! target.getGameMode().equals( GameMode.CREATIVE ) ) {
+
                             target.setGameMode( GameMode.CREATIVE );
                             target.sendMessage( "§fYour Gamemode was updated!" );
                             sender.sendMessage( "§fThe player §6" + target.getName() + " §fis now in the §6" + target.getGameMode() + " §fmode." );
@@ -70,6 +76,7 @@ public class GameModeCommand implements CommandExecutor {
                     }
                     if( args[0].equalsIgnoreCase( "2" ) || args[0].equalsIgnoreCase( "adventure" ) ) {
                         if( ! target.getGameMode().equals( GameMode.ADVENTURE ) ) {
+
                             target.setGameMode( GameMode.ADVENTURE );
                             target.sendMessage( "§fYour Gamemode was updated!" );
                             sender.sendMessage( "§fThe player §6" + target.getName() + " §fis now in the §6" + target.getGameMode() + " §fmode." );
@@ -79,6 +86,7 @@ public class GameModeCommand implements CommandExecutor {
                     }
                     if( args[0].equalsIgnoreCase( "3" ) || args[0].equalsIgnoreCase( "spectator" ) ) {
                         if( ! target.getGameMode().equals( GameMode.SPECTATOR ) ) {
+
                             target.setGameMode( GameMode.SPECTATOR );
                             target.sendMessage( "§fYour Gamemode was updated!" );
                             sender.sendMessage( "§fThe player §6" + target.getName() + " §fis now in the §6" + target.getGameMode() + " §fmode." );
