67c75696576dddf2693966efe114dbbcb78e10ed
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-03-29 18:23:53 +0200

    Update PlayerQuitListener.java

--- GItHubSpigotCoding/src/de.derkaottv/listeners/PlayerQuitListener.java
+++ GItHubSpigotCoding/src/de.derkaottv/listeners/PlayerQuitListener.java
@@ -1,16 +1,36 @@
 package de.derkaottv.listeners;
 
+import de.derkaottv.utils.Scoreboard;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerQuitListener implements Listener {
+
+    private de.derkaottv.main plugin;
+
+    public PlayerQuitListener(de.derkaottv.main main) {
+        this.plugin = main;
+    }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
         String name = p.getName();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+
+                for (Player all : Bukkit.getOnlinePlayers()) {
+                    Scoreboard.setScoreboard(all);
+                }
+
+            }
+        }.runTaskLater(plugin, 1);
 
         if( p.isOp() ) {
 
